5fddaf4709da5ddba8201886e79b0ec5bca24d95
username <useremail>
2024-05-19 15:57:13 -0700

    changed wait function to a "Story" function allowing an optimization for moments where waiting for player.

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -10,8 +10,9 @@ def delete_last_line(n=1):
         sys.stdout.write('\x1b[1A') #Move cursor up a line
         sys.stdout.write('\x1b[2K') #Deletes the line
 
-#Function to use delete_last_line to wait for player        
-def wait():
+#Function to print a line then wait for player to continue. Uses above "delete last line" function to delete instruction line.       
+def story(line):
+    print(f"{line}")
     input(f"Press Enter to continue")
     delete_last_line(1)
 
@@ -19,8 +20,7 @@ def wait():
 print(f"Welcome to 'A Gnome and his Hat'.")
 print(f"Here you will partake in a tale of a Gnome who's lost his hat.")
 print(f"Before we begin, I'll ask you a few questions.")
-print(f"After typing your answer, be sure to press the enter key.\n")
-wait()
+story("After typing your answer, be sure to press the enter key.\n")
 
 #5 questions to fill variables.
 character = input(f"What is the gnome's name? ").strip()
@@ -57,4 +57,12 @@ if favoriteThing.lower() == "something":
 
 input(f"Press Enter to begin the story")
 
-os.system('cls') #clears screen to start the story on a fresh page
+os.system('cls') #clears screen to start the story on a fresh page, this also doesn't work from the IDE.
+
+#Title banner
+print("-"*20)
+print("A Gnome and his Hat")
+story("-"*20,"\n")
+
+#Story Intro
+story("Our story begins here in a lush garden as dusk begins to set. Sunlight barely peeking from the western horizon.")
