4e2c748992eb4578cf38b775ad4595588931d9dc
username <useremail>
2024-05-19 18:04:21 -0700

    partial story intro. saving for safety, coding on VM and the internet briefly cut.

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -20,39 +20,40 @@ def story(line):
 print(f"Welcome to 'A Gnome and his Hat'.")
 print(f"Here you will partake in a tale of a Gnome who's lost his hat.")
 print(f"Before we begin, I'll ask you a few questions.")
-story("After typing your answer, be sure to press the enter key.\n")
+story(f"After typing your answer, be sure to press the enter key.\n")
 
 #5 questions to fill variables.
 character = input(f"What is the gnome's name? ").strip()
 while len(character) == 0:
     character = input(f"The gnome cannot be nameless, please enter a name: ").strip()
+character = character[0].upper() + character[1:] #Names should be capitalized
 
 hair = input(f"While we all know gnome's have white beard's, what color is {character}'s hair? ").strip()
 while len(hair) == 0:
     hair = input(f"Please enter a color: ").strip()
 
-hatColor = input(f"What color is {character}'s hat?").strip()
+hat_color = input(f"What color is {character}'s hat?").strip()
 corrected = False #whether or not player was corrected
-while hatColor.lower() == "colorless" or len(hatColor) == 0:
-    if hatColor.lower() == "colorless" and corrected:
-        hatColor = input(f"You think you're funny, don't you? Enter a color: ").strip()
+while hat_color.lower() == "colorless" or len(hat_color) == 0:
+    if hat_color.lower() == "colorless" and corrected:
+        hat_color = input(f"You think you're funny, don't you? Enter a color: ").strip()
     else:
-        hatColor = input(f"A gnome's hat is important, it can't be colorless! Enter a color: ").strip()
+        hat_color = input(f"A gnome's hat is important, it can't be colorless! Enter a color: ").strip()
         corrected = True
 
-hatMaterial = input(f"What is the material of {character}'s hat? ").strip()
+hat_material = input(f"What is the material of {character}'s hat? ").strip()
 corrected = False
-while hatMaterial.lower() == "nothing" or len(hatMaterial) == 0:
-    if hatMaterial.lower() == "nothing" and corrected > 0:
-        hatMaterial = input(f"Funny guy, huh? C'mon, we don't have all day... or do we? Enter a material: ").strip()
+while hat_material.lower() == "nothing" or len(hat_material) == 0:
+    if hat_material.lower() == "nothing" and corrected > 0:
+        hat_material = input(f"Funny guy, huh? C'mon, we don't have all day... or do we? Enter a material: ").strip()
     else:
-        hatMaterial = input(f"You can't make a hat out of nothing! Enter a material: ").strip()
+        hat_material = input(f"You can't make a hat out of nothing! Enter a material: ").strip()
         corrected = True
 
-favoriteThing = input(f"What is {character}'s favorite thing? What gives {character} comfort? ").strip()
-while len(favoriteThing) == 0:
-    favoriteThing = input(f"There must be SOMETHING {character} loves! Enter a favorite thing: ").strip()
-if favoriteThing.lower() == "something":
+favorite_thing = input(f"What is {character}'s favorite thing? What gives {character} comfort? ").strip()
+while len(favorite_thing) == 0:
+    favorite_thing = input(f"There must be SOMETHING {character} loves! Enter a favorite thing: ").strip()
+if favorite_thing.lower() == "something":
     print(f"Okay funny guy. You know what? You're stuck with it now.")
 
 input(f"Press Enter to begin the story")
@@ -62,7 +63,14 @@ os.system('cls') #clears screen to start the story on a fresh page, this also do
 #Title banner
 print("-"*20)
 print("A Gnome and his Hat")
-story("-"*20,"\n")
+print("-"*20,"\n")
+story(" ")
 
 #Story Intro
-story("Our story begins here in a lush garden as dusk begins to set. Sunlight barely peeking from the western horizon.")
+story(f"Our story begins here in a lush garden as dusk begins to set. Sunlight barely peeking from the western horizon.")
+story(f"{character} awakens with a yawn and a stretch. Invigorated in a way that only 'morning people' understand.")
+story(f"{character} wipes the sleep from his eyes as he enjoys the slight breeze enveloping him.")
+story(f"'The wind feels great blowing through my lush {hair},' {character} thinks to himself...")
+story(f"Or he would if he had any hair. Instead the rush of cold air across his balding head brings him to a frightening conclusion...")
+story(f"'MY HAT! MY HAT IS GONE!' {character} exclaims with great urgence!")
+story(f"He searches frantically for his hat throughout the garden. But try as he might, he can't find that deep {hat_color} hat. Not even a trace of {hat_material}!")
